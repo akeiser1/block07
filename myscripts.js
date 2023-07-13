3c3a7d67fe0b7afaeb677ce4c50e3ca5831e46e3
Aaron Keiser <akeiser1@gmail.com>
2023-07-12 19:49:23 -0500

    first commit

---
+++ myscripts.js
@@ -0,0 +1,16 @@
+/*
+1. Declare all variables.
+2. Write arithmetic operators as such they create combination 10-40-39.
+3. Create code that will generate working HTML page.
+*/
+
+let message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+console.log(message);
+
+let sum= 5 + 5; //Declare x, give it value of 5 + 5
+let diff = 70 - 30; //Declare y, give it value of 70 - 30
+let prod = 3 * 13; //Declare z, give it value of 3 * 13
+
+console.log(`You have received this message because you have been chosen to open an important vault.
+Here is the secret combination: ${sum} - ${diff} - ${prod}`);
